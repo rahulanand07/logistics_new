3649c077fd2324ec5047414f280e6fdddb37daf3
amit <amitadd99@gmail.com>
2024-04-08 15:38:09 +0530

    task model as well as other task

--- router/patient_router.js
+++ router/patient_router.js
@@ -1,10 +1,11 @@
-import { test_api } from "../controller/patient_controller.js";
+import { test_api,get_patient_by_id } from "../controller/patient_controller.js";
 
 import { Router } from "express";
 
 const router = Router()
 
 router.get("/test",test_api)
+router.get("/patient/patient-by-id/:id",get_patient_by_id)
 
 
 export {router}
